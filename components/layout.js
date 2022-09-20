272c0126e839606d96cbc783231b6eeec0b8aa2a
aaron mitchell <aaronmitchell9902@gmail.com>
2022-09-20 00:19:34 -0400

    added-bootcamp-post

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Aaron Mitchell';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Aaron Mitchell - Dev';
 
 export default function Layout({ children, home }) {
   return (
